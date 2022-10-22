d5fa811876342eb53b3a62e6d35bba389c945646
Finn Formica <finnformica@gmail.com>
2022-10-23 00:29:53 +0100

    update navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -40,13 +40,13 @@ const Header = () => (
         </Link>
       </li>
       <li>
-        <Link href="#tech">
-          <NavLink>Technologies</NavLink>
+        <Link href="#about">
+          <NavLink>About</NavLink>
         </Link>
       </li>
       <li>
-        <Link href="#about">
-          <NavLink>About</NavLink>
+        <Link href="/blog">
+          <NavLink>Blog</NavLink>
         </Link>
       </li>
     </Div2>
