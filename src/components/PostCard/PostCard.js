2ad9f623ee4b0f55dc758f9ef1d2de5444a57d7d
Finn Formica <finnformica@gmail.com>
2022-10-22 18:41:53 +0100

    add card for blogs

--- src/components/PostCard/PostCard.js
+++ src/components/PostCard/PostCard.js
@@ -10,22 +10,28 @@ import {
   Img,
 } from "./PostCardStyles";
 
-const PostCard = ({ post, id }) => {
-  return (
-    <BlogCard key={id}>
-      <Img src={post.frontmatter.cover_image} />
-      <TitleContent>
-        <HeaderThree title="true">{post.frontmatter.title}</HeaderThree>
-        <Hr />
-      </TitleContent>
-      <CardInfo>{post.frontmatter.excerpt}</CardInfo>
-      <div style={{ margin: "4rem 0 4rem 0" }}>
-        <ExternalLinks target="_blank" href="#">
-          Read more
-        </ExternalLinks>
-      </div>
-    </BlogCard>
-  );
-};
+const PostCard = ({
+  post: {
+    slug,
+    frontmatter: { cover_image, title, excerpt, date },
+  },
+}) => (
+  <BlogCard>
+    <Img src={cover_image} />
+    <TitleContent>
+      <HeaderThree title="true">{title}</HeaderThree>
+      <Hr />
+    </TitleContent>
+    <CardInfo>{excerpt}</CardInfo>
+    <div style={{ margin: "4rem 0 4rem 0" }}>
+      <strong style={{ color: "#b9d9eb", marginRight: "40px" }}>
+        <span>{date}</span>
+      </strong>
+      <Link href={`/blog/${slug}`}>
+        <ExternalLinks>Read more</ExternalLinks>
+      </Link>
+    </div>
+  </BlogCard>
+);
 
 export default PostCard;
