2e9a77820401c78db3443b4aa75f73cb1dd38871
Finn Formica <finnformica@gmail.com>
2022-10-27 17:08:17 +0100

    add favicon

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,7 +5,7 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        {/* <link rel="icon" href="/public/favicon.ico" /> */}
+        <link rel="icon" href="/favicon.png" />
         <title>Finn Formica</title>
       </Head>
       <Theme>
